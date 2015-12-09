3b81b91f80894adae4c8e779d004bbf37e72ac73
André Sääf <bazetts@gmail.com>
2015-12-09 17:04:25 +0100

    jQuery & Structure

--- IRC_Clone/index.php
+++ IRC_Clone/index.php
@@ -1,3 +1,11 @@
-<?php
-	echo "Hello World!"
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Wow! So cool!</title>
+    <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="js/chat.js"></script>
+</head>
+<body>
+    <?php echo 'Hello'; ?>
+</body>
+</html>
