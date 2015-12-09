8c4981f6a8d99d4f517f4caf56c4e132b4b7b71c
AlvinSamuelsson <alvin.samuelsson@gmail.com>
2015-12-09 17:33:50 +0100

    Added bootstrap
    # Conflicts:
    #	IRC_Clone/index.php

--- IRC_Clone/index.php
+++ IRC_Clone/index.php
@@ -4,6 +4,7 @@
     <title>Wow! So cool!</title>
     <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
     <script type="text/javascript" src="js/chat.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 </head>
 <body>
     <?php echo 'Hello'; ?>
