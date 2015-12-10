05c5fe7890109a339ee5a884c381a34bd548c9d7
AlvinSamuelsson <alvin.samuelsson@gmail.com>
2015-12-10 18:17:12 +0100

    Added ALOT of HTML, forms, text, etc.  Added CSS too!

--- IRC_Clone/index.php
+++ IRC_Clone/index.php
@@ -1,43 +1,59 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Wow! So cool!</title>
-    <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
-    <script type="text/javascript" src="js/chat.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="css/style.css" />
-</head>
-<body>
+    <head>
+        <title>Wow! So cool!</title>
+        <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+        <script type="text/javascript" src="js/chat.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/style.css" />
+    </head>
+    <body>
 
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Project name</a>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Project #IRC sexy</a>
+                </div>
+                <div id="navbar" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
             </div>
-            <div id="navbar" class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-            </div><!--/.nav-collapse -->
+        </nav>
+        <div class="container top-buffer" id="content">
+            <div class="row">        
+                <div class="col-md-5 text-center col-md-offset-3" id="grey-text">
+                        <h1>This is our chat</h1>
+                        <p class="lead">There are many chats<br> But this one is mine. Such cool chat, <br> now meme free! Try it out!</p>
+                </div>
+            <div class="col-md-2 col-md-offset-1" id="grey-text">        
+                    <form action="get" class="form-horizontal">
+                        <div class="form-group">
+                            <label for="username_input"> Username </label>
+                            <br>
+                            <input class="form-control" type="text" name="username" id="username">
+                        </div>
+                        <div class="form-group">
+                            <label for="password_input"> Password </label>
+                            <br>
+                            <input class="form-control" type="text" name="password" id="password">
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-default">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-    </nav>
-
-    <div class="container">
-
-        <div class="starter-template">
-            <button class="btn btn-lg btn-primary" id="primaryButton">Primary</button>
-            <h1>Bootstrap starter template</h1>
-            <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-        </div>
-
-    </div>
-</body>
+    </body>
 </html>
