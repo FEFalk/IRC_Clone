a578b7bcf858339d4f7131bf84ab29ad5d56694d
AlvinSamuelsson <alvin.samuelsson@gmail.com>
2015-12-10 18:23:22 +0100

    fixed github merging fail
    :+1:

--- IRC_Clone/index.php
+++ IRC_Clone/index.php
@@ -1,16 +1,5 @@
 <!DOCTYPE html>
 <html>
-<<<<<<< HEAD
-    <head>
-        <title>Wow! So cool!</title>
-        <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
-        <script type="text/javascript" src="js/chat.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="css/style.css" />
-    </head>
-    <body>
-=======
 <head>
     <title>Wow! So cool!</title>
     <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
@@ -20,8 +9,6 @@
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700,800' rel='stylesheet' type='text/css'>
 </head>
 <body>
->>>>>>> origin/master
-
         <nav class="navbar navbar-inverse navbar-fixed-top">
             <div class="container">
                 <div class="navbar-header">
@@ -66,28 +53,6 @@
                     </form>
                 </div>
             </div>
-<<<<<<< HEAD
         </div>
     </body>
-=======
-            <div id="navbar" class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-            </div><!--/.nav-collapse -->
-        </div>
-    </nav>
-
-    <div class="container">
-
-        <div class="starter-template">
-            
-            <h1 id="homeTitle">IRC-CLONE</h1>
-            <!--<button class="btn btn-lg btn-primary" id="primaryButton">Primary</button>-->
-        </div>
-    </div>
-</body>
->>>>>>> origin/master
 </html>
